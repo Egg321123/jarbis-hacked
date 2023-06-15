ee9e44c87349512c4c9de9c971256a9055e38556
Gido <130654343+Egg321123@users.noreply.github.com>
2023-06-15 09:48:50 +0200

    Add files via upload

---
+++ Person.cs
@@ -0,0 +1,11 @@
+﻿using System;
+public class Person
+{
+    public string FirstName;
+    public string LastName;
+    public int Age;
+    public string Introduction()
+    {
+        return ($"My first name is: {FirstName} \n My last name is: {LastName} \n My age is: {Age}");
+    }
+}
